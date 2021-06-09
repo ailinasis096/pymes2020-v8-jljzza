03d551b7660a965cd547c61c97c3b8d489030d27
ailinasis096 <69639@sistemas.frc.utn.edu.ar>
2021-06-09 15:23:12 -0300

    Initial commit

---
+++ src/app/models/servicios.ts
@@ -0,0 +1,14 @@
+export class Servicio {
+  IdServicio: number;
+  Descripcion: string;
+  Importe: number;
+  CantidadHoras: number;
+}
+export const Servicios: Servicio[] = [
+  {
+    IdServicio: 1,
+    Descripcion: 'Algo',
+    Importe: 10.5,
+    CantidadHoras: 2
+  }
+];
